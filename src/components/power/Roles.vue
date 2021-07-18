b846386397aad067d934b4922e5e1eda732dada0
Jeff Lu <373054393@qq.com>
2021-07-19 00:19:24 +0800

    添加增加角色功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -10,7 +10,9 @@
     <el-card>
       <el-row>
         <el-col>
-          <el-button type="primary" >添加角色</el-button>
+          <el-button type="primary" @click="addDialogVisible = true"
+            >添加角色</el-button
+          >
         </el-col>
       </el-row>
 
@@ -46,6 +48,40 @@
         </el-table-column>
       </el-table>
     </el-card>
+
+    <!-- 添加角色的对话框 -->
+    <el-dialog
+      title="添加角色"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addRoleClosed"
+    >
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="80px"
+      >
+        <el-form-item
+          label="角色名称"
+          :label-width="formLabelWidth"
+          prop="roleName"
+        >
+          <el-input v-model="addForm.roleName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="角色描述"
+          :label-width="formLabelWidth"
+          prop="roleDesc"
+        >
+          <el-input v-model="addForm.roleDesc" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addRole">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -54,6 +90,20 @@ export default {
   data() {
     return {
       rolesList: [],
+      addDialogVisible: false,
+      formLabelWidth: "120px",
+      addForm: {
+        roleName: "",
+        roleDesc: "",
+      },
+      addFormRules: {
+        roleName: [
+          { required: true, message: "请输入角色名称", trigger: "blur" },
+        ],
+        roleDesc: [
+          { required: true, message: "请输入角色描述", trigger: "blur" },
+        ],
+      },
     };
   },
   created() {
@@ -63,10 +113,30 @@ export default {
   methods: {
     async getRolesList() {
       const { data: res } = await this.$http.get("roles");
-      if(res.meta.status !== 200){
-        return this.$message.error('获取角色列表失败')
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取角色列表失败");
       }
       this.rolesList = res.data;
+    },
+    // 添加角色
+
+    addRole() {
+      this.$refs.addFormRef.validate(async (validate) => {
+        if (!validate) {
+          return;
+        }
+        const { data: res } = await this.$http.post("roles", this.addForm);
+        if (res.meta.status !== 201) {
+          return this.$message.error("添加角色列表失败");
+        }
+        this.addDialogVisible = false;
+        this.getRolesList();
+        this.$message.success("添加角色成功！");
+      });
+    },
+    // 监听添加角色对话框的关闭事件
+    addRoleClosed() {
+      this.$refs.addFormRef.resetFields();
     },
   },
 };
