3531470dd8d891af9c140f13c00bd8f1290ace0a
Jeff Lu <373054393@qq.com>
2021-07-23 00:43:54 +0800

    实现商品分类展示

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-button type="primary">添加分类</el-button>
+
+      <tree-table
+        class="tree-table"
+        :data="cateList"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        show-index
+        border
+      >
+        <template slot="isOk" slot-scope="scope">
+          <i
+            class="el-icon-error"
+            style="color: red"
+            v-if="scope.row.cat_deleted"
+          ></i>
+          <i class="el-icon-success" style="color: lightgreen" v-else></i>
+        </template>
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+          <el-tag
+            size="mini"
+            type="success"
+            v-else-if="scope.row.cat_level === 1"
+            >二级</el-tag
+          >
+          <el-tag size="mini" type="warning" v-else>三级</el-tag>
+        </template>
+        <!-- 操作 -->
+        <template slot="operate" slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            class="el-icon-edit"
+            @click="showEditDialog(scope.row.cat_id)"
+            >编辑</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            class="el-icon-delete"
+            @click="removeCateById(scope.row.cat_id)"
+            >删除</el-button
+          >
+        </template>
+      </tree-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 查询分类列表的参数对象
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5,
+      },
+      cateList: [],
+      total: 0,
+      columns: [
+        {
+          label: "分类名称",
+          prop: "cat_name",
+        },
+        {
+          label: "是否有效",
+          type: "template",
+          template: "isOk",
+        },
+        {
+          label: "排序",
+          type: "template",
+          template: "order",
+        },
+        {
+          label: "操作",
+          // 将当前列定义为模板列
+          type: "template",
+          // 当前列使用的模板名称
+          template: "operate",
+        },
+      ],
+    };
+  },
+  created() {
+    this.getCateList();
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get("categories", {
+        params: this.queryInfo,
+      });
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取商品分类失败");
+      }
+      this.cateList = res.data.result;
+      this.total = res.data.total;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
